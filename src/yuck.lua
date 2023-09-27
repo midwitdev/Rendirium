3aaf1c2ebb88e3028ddc1ccdae6c32af17b99373
Andrew <js0861311@gmail.com>
2023-09-27 19:22:06 -0400

    Created a window

--- src/yuck.lua
+++ src/yuck.lua
@@ -1,11 +1,11 @@
 local ffi = require "ffi"
-local engine = ffi.load "./libYuck.so"
+local engine = ffi.load "./libyuck.so"
 local yuck = {}
 
 -- the got dam GLFW3
 ffi.cdef[[
     void puts(char*);
-    struct GLFWwindow glfwCreateWindow(int width, int height, const char *title, struct GLFWmonitor *monitor, struct GLFWwindow *share);
+    struct GLFWwindow* glfwCreateWindow(unsigned int width, unsigned int height, char *title, struct GLFWmonitor *monitor, struct GLFWwindow *share);
     void glfwWindowHint(int hint, int value);
     int glfwWindowShouldClose(struct GLFWwindow* window);
     void glfwPollEvents();
@@ -14,14 +14,21 @@ ffi.cdef[[
     void glfwDestroyWindow(struct GLFWwindow* window);
     
     enum {
-        GLFW_FALSE,
-        GLFW_TRUE,
-    }
+        GLFW_FALSE = 0,
+        GLFW_TRUE = 1,
+
+        GLFW_CLIENT_API             = 0x00022001,
+        GLFW_NO_API                 = 0x0,
+
+        GLFW_CONTEXT_VERSION_MAJOR  = 0x00022002,
+        GLFW_CONTEXT_VERSION_MINOR  = 0x00022003,
+        GLFW_RESIZABLE              = 0x00020003,
+    };
 ]]
 
 
 
-function cs(s)
+function cstr(s)
     return ffi.new("char[?]", #s+1, s)
 end
 
@@ -29,16 +36,28 @@ function ls(cstr)
     return ffi.string(cstr)
 end
 
+function cbool(cbool)
+    return cbool == 1
+end
+
 yuck.log = print
 
-engine.puts(cs(ls(cs"test")));
-
+print'0'
 engine.glfwInit();
+print'1'
 engine.glfwWindowHint(engine.GLFW_CLIENT_API, engine.GLFW_NO_API);
+print'2'
 engine.glfwWindowHint(engine.GLFW_RESIZABLE, engine.GLFW_FALSE);
-local window = engine.glfwCreateWindow(800, 600, "Test", nil, nil);
-engine.glfwTerminate();
+print'3'
+local window = engine.glfwCreateWindow(800, 600, cstr"Test", nil, nil);
+print'4'
 
--- YAY!!!!!!!!!
+while cbool(engine.glfwWindowShouldClose(window)) == false do
+    engine.glfwPollEvents();
+end
+
+print("Terminating window");
+engine.glfwTerminate();
+print'5'
 
 -- return yuck
